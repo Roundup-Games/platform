89059ef00460ffb8c3b4c9935a88b101f436c94c
Hosted Weblate <hosted@weblate.org>
2026-07-24 11:13:17 +0200

    Added translation using Weblate (Spanish)
    Co-authored-by: Decoded Notes <decodednotes.studio@gmail.com>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>

---
+++ lang/es/plan.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'seo_title' => '',
+    'seo_description' => '',
+    'heading_plan_something' => '',
+    'content_choose_frequency' => '',
+    'content_how_often' => '',
+    'content_one_time' => '',
+    'content_one_time_desc' => '',
+    'content_recurring' => '',
+    'content_recurring_desc' => '',
+    'content_smart_defaults_hint' => '',
+    'nav_plan_something' => '',
+    'action_plan_something' => '',
+];
